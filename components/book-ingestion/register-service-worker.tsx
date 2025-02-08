b909913eb7c5151d0ecf65d7f44439ac54b4aff5
Jonathan Woahn <jonathanwoahn@gmail.com>
2025-02-08 07:42:25 -0700

    removes comments

--- components/book-ingestion/register-service-worker.tsx
+++ components/book-ingestion/register-service-worker.tsx
@@ -3,35 +3,9 @@
 import { useEffect } from "react";
 
 export const SERVICE_WORKER_PATH = '/ingestion.worker.js';
-
 export default function RegisterServiceWorker() {
 
   useEffect(() => {
-    // const startJobProcessing = () => {
-    //   if(navigator.serviceWorker.controller) {
-    //     console.log('send message startProcessing');
-    //     navigator.serviceWorker.controller.postMessage( {task: 'startProcessing'});
-    //   }
-    // }
-    
-    // const registerServiceWorker = async () => {
-    //   if ('serviceWorker' in navigator) {
-    //     const registration = await navigator.serviceWorker.getRegistration(SERVICE_WORKER_PATH);
-
-    //     if (!registration) {
-    //       // navigator.serviceWorker.register(SERVICE_WORKER_PATH)
-    //       navigator.serviceWorker.register(SERVICE_WORKER_PATH, { type: 'module' })
-    //         .then((registration) => {
-    //           console.log('Service Worker registered with scope:', registration.scope);
-    //         })
-    //         .catch((error) => {
-    //           console.error('Service Worker registration failed:', error);
-    //         });
-    //     }else {
-    //       // startJobProcessing();
-    //     }
-    //   }
-    // };
     const registerServiceWorker = async () => {
       if ('serviceWorker' in navigator) {
         try {
@@ -48,7 +22,6 @@ export default function RegisterServiceWorker() {
         }
       }
     };
-    
 
     registerServiceWorker();
   }, []);
